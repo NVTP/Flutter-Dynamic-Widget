7d0bbfd319f59f2faca20283824a6983a5872d5b
naravut <s5952410015@sau.ac.th>
2020-02-03 01:04:32 +0700

    listview.builder

---
+++ lib/main.dart
@@ -0,0 +1,164 @@
+import 'package:flutter/material.dart';
+import 'package:flutter_app1111/views/dynamic_widget.dart';
+
+void main() => runApp(
+  MaterialApp(
+    home: DynamicWidget(),
+  ),
+);
+
+class TimeTrackApp extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return new MaterialApp(
+      title: 'Time Tracker',
+      theme: new ThemeData(
+        primarySwatch: Colors.blue,
+      ),
+      home: new TimeTrackHome(title: 'Task List'),
+    );
+  }
+}
+
+class TimeTrackHome extends StatefulWidget {
+  TimeTrackHome({Key key, this.title}) : super(key: key);
+
+  final String title;
+
+  @override
+  _TimeTrackHomeState createState() => new _TimeTrackHomeState();
+}
+
+class _TimeTrackHomeState extends State<TimeTrackHome> {
+  TextEditingController _textController;
+  List<TaskItem> _taskList = new List<TaskItem>();
+
+  void _addTaskDialog() async {
+    _textController = TextEditingController();
+
+    await showDialog(
+        context: context,
+        builder: (_) => new AlertDialog(
+          title: new Text("Add A New Task"),
+          content: new TextField(
+            controller: _textController,
+            decoration: InputDecoration(
+                border: InputBorder.none, hintText: 'Enter the task name'),
+          ),
+          actions: <Widget>[
+            new FlatButton(
+                onPressed: () => Navigator.pop(context),
+                child: const Text("CANCEL")),
+            new FlatButton(
+                onPressed: (() {
+                  Navigator.pop(context);
+                  _addTask(_textController.text);
+                }),
+                child: const Text("ADD"))
+          ],
+        ));
+  }
+
+  void _addTask(String title) {
+    setState(() {
+      // add the new task
+      _taskList.add(TaskItem(
+        name: title,
+      ));
+    });
+  }
+
+  @override
+  void initState() {
+    _taskList = List<TaskItem>();
+    super.initState();
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return new Scaffold(
+      appBar: new AppBar(
+        title: new Text(widget.title),
+      ),
+      body: new Align(
+        alignment: Alignment.topCenter,
+        child: ListView.builder(
+            padding: EdgeInsets.symmetric(horizontal: 20),
+            itemExtent: 60.0,
+            itemCount: _taskList.length,
+            itemBuilder: (BuildContext context, int index) {
+              if (index < _taskList.length) {
+                return Dismissible(
+                  key: ObjectKey(_taskList[index]),
+                  onDismissed: (direction) {
+                    if(this.mounted) {
+                      setState(() {
+                        _taskList.removeAt(index);
+                      });
+                    }
+                  },
+                  child: _taskList[index],
+                );
+              }else{
+                return null;
+              }
+            }),
+      ),
+      floatingActionButton: new FloatingActionButton(
+        onPressed: _addTaskDialog,
+        tooltip: 'Click to add a new task',
+        child: new Icon(Icons.add),
+      ),
+    );
+  }
+}
+
+class TaskItem extends StatefulWidget {
+  final String name;
+
+  TaskItem({Key key, this.name}) : super(key: key);
+  TaskItem.from(TaskItem other) : name = other.name;
+
+  @override
+  State<StatefulWidget> createState() => new _TaskState();
+}
+
+class _TaskState extends State<TaskItem> {
+  static final _taskFont =
+  const TextStyle(fontSize: 26.0, fontWeight: FontWeight.bold);
+  Color _color = Colors.transparent;
+
+  void _highlightTask() {
+    setState(() {
+      if(_color == Colors.transparent) {
+        _color = Colors.greenAccent;
+      }
+      else {
+        _color = Colors.transparent;
+      }
+    });
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Column(children: <Widget>[
+      Material(
+        color: _color,
+        child: ListTile(
+          title: Text(
+            widget.name,
+            style: _taskFont,
+            textAlign: TextAlign.center,
+          ),
+          onTap: () {
+            _highlightTask();
+          },
+        ),
+      ),
+      Divider(
+        height: 0.0,
+        color: Colors.red,
+      ),
+    ]);
+  }
+}
---
+++ lib/views/dynamic_widget.dart
@@ -0,0 +1,87 @@
+import 'package:flutter/material.dart';
+
+class DynamicWidget extends StatefulWidget {
+  @override
+  _DynamicWidgetState createState() => _DynamicWidgetState();
+}
+
+class _DynamicWidgetState extends State<DynamicWidget> {
+  TextEditingController  _eCtrl;
+  List<String> textList = [];
+  bool showDialog = false;
+
+  @override
+  void initState() {
+    // TODO: implement initState
+    _eCtrl = TextEditingController();
+    super.initState();
+  }
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: SafeArea(
+        child: Column(
+          children: <Widget>[
+            Container(
+              alignment: Alignment.topRight,
+              padding: EdgeInsets.symmetric(horizontal: 20),
+              child: RaisedButton(
+                onPressed: (){
+                  setState(() {
+                    showDialog = true;
+                  });
+                },
+                shape: RoundedRectangleBorder(
+                  borderRadius: BorderRadius.circular(20)
+                ),
+                elevation: 1.1,
+                child: Text('Add Color'),
+                color: Colors.blueGrey[300],
+              ),
+            ),
+            showDialog == true ?
+            AlertDialog(
+              title: Text('Alert'),
+              content: TextField(
+                controller: _eCtrl,
+                decoration: InputDecoration.collapsed(hintText: 'Color'),
+                maxLines: 1,
+                onSubmitted: (String text){
+
+                },
+              ),
+              actions: <Widget>[
+                FlatButton(
+                  onPressed: (){
+                    setState(() {
+                      showDialog = false;
+                      textList.add(_eCtrl.text);
+                      _eCtrl.clear();
+                    });
+                  },
+                  child: Text('OK'),
+                ),
+              ],
+            ) : Text(''),
+            Flexible(
+              child: ListView.builder(
+                itemCount: textList.length,
+                itemBuilder: (BuildContext context, int Index){
+                  return Row(
+                    children: <Widget>[
+                      Checkbox(
+                        value: true,
+                        onChanged: null,
+                      ),
+                      Text(textList[Index])
+                    ],
+                  );
+                },
+              ),
+            ),
+          ],
+        ),
+      ),
+    );
+  }
+}
